b0f3b0d31b756d3bab90e53fb921543ba91a91bd
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-05-07 23:50:10 -0300

    Udemy course about R

---
+++ Udemy/ajuda.R
@@ -0,0 +1,22 @@
+#ajuda dentro do R
+
+help.start() #ajuda geral do R
+
+help("*")
+
+help(mean)
+
+help.search("mean")
+
+
+?sd #mesmo que help
+??sd #mesmo que help.search
+
+apropos("help") #funções contendo "help"
+apropos("mean")
+
+#exemplos de uso
+example(mean)# exemplo de uso
+
+example(plot)
+
---
+++ Udemy/funcoes_basicas.R
@@ -0,0 +1,36 @@
+#Vetor
+#sequência de elementos do mesmo tipo
+#uma dimensão
+
+x<- c(1,3,5,6,8)
+
+m = 2:20
+
+#operações com vetores
+x+2
+
+x/2
+
+x * 2
+
+m + 3
+
+#média
+mean(x)
+mean(m)
+
+#mediana
+median(x)
+median(m)
+
+#soma
+sum(x)
+sum(m)
+
+#desvio padrão
+sd(x)
+
+
+#boxplot
+boxplot(x)
+boxplot(m)
