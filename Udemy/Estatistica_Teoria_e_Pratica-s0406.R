b9e0b9becb575678e631547b3110d7a758458e82
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-05-25 08:17:24 -0300

    Estatistica: Teoria e Pratica - s04 a06

---
+++ Udemy/Estatistica_Teoria_e_Pratica-s0406.R
@@ -0,0 +1,45 @@
+jogadores = c(40000,18000,12000,250000,30000,140000,300000,40000,800000)
+
+mean(jogadores)
+
+median(jogadores)
+
+quartis = quantile(jogadores)
+quartis
+quartis[4]
+
+sd(jogadores)
+
+range(jogadores)
+
+summary(jogadores)
+
+# Quiz
+
+alunos = c(12,14,11,12,15,16,13,17,13,14)
+mean(alunos)
+sd(alunos)
+summary(alunos)
+
+numeros = c(3,  1,  4,  7 , 9,  2, 10,  5,  6,  8)
+
+salarios = c(4000, 5000, 3000, 5000, 4500)
+sum(salarios)/length(salarios)
+mean(salarios)
+
+numeros6 = c(3,  1,  4,  7 , 9,  2, 10,  5,  6,  8)
+median(numeros6)
+
+numeros7 = c(12,14,11,12,16,13,17,13,14)
+median(numeros7)
+
+numeros8 = c(12, 14, 11, 12, 16, 13, 17, 13, 14)
+quantile(numeros8)
+
+numeros9 = c(14, 11, 12, 16, 13, 17, 13, 14)
+max(numeros9)-min(numeros9)
+
+
+
+
+
