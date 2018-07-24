10e44cf5b7ea8ae06239714e8f77c0f0306f9922
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-07-24 13:44:26 -0300

    Functions tests

--- test1.R
+++ test1.R
@@ -1,22 +1,17 @@
+#
+# Testes sobre a linguagem R
+#
 
-trd = c(9, 16, 6, 49, 21, 16, 8)
-median(trd)
-deviance(trd)
+funSimpleParm <- function(x) { x * 10 + 1 }
+funDoubleParm <- function(x, y) { x * 100 + y }
 
-pi
-
-estimaBB <- function(distAsa, valorAcao, volDiaria, diasVencimento) {
-  estBB <- distAsa / (pi * valorAcao * volDiaria * sqrt(diasVencimento))
-  
-  print(estBB)
-}
-
-volAno = 46.22 / 100
-volDia = volAno / sqrt(252)
-bb <- c(30:6)
-bb
-bbEst <- estimaBB(1, 20, volDia, bb)
-plot(bbEst)
-
-valorBB = estimaBB(1, 19, 0.0291189209627, 20)
-valorBB = estimaBB(1, 19, volDia, 20)
+funSimpleParm(10)
+funDoubleParm(1, 2)
+sqRange1 <- seq(1, 10)
+sqRange2 <- seq(1, 20, 2)
+sqRange1
+sqRange2 
+funSimpleParm(sqRange1)
+funDoubleParm(sqRange1, sqRange2)
+funDoubleParm(12, sqRange2)
+funDoubleParm(sqRange1, 34)
