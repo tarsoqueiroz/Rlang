ed5e3d26258146261f296d664ddeca4e87c218dd
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-07-26 16:54:32 -0300

    End of Section 2

--- Udemy/Formacao Cientista de Dados/Secao 02 - Estatistica I.R
+++ Udemy/Formacao Cientista de Dados/Secao 02 - Estatistica I.R
@@ -103,6 +103,7 @@ probDadoJusto(100000)
 probDadoJusto(100000)
 probDadoJusto(100000)
 
+#
 # Distribuição
 #
 
@@ -139,6 +140,158 @@ pbinom(numSinaisVerdes, numSinais, probSinais)
 # prova 12 questões, acertar 7 questões com 4 alternativas cada
 
 dbinom(7, 12, 0.25)
+
+# Score Z / Valor Z - número de desvios padrões de distância da média
+
+# Como saber se é Normal?
+# Gerar histograma
+
+?rnorm()
+?rbeta()
+?qqnorm()
+?qqline()
+?shapiro.test()
+
+amostra <- rbeta(10000, 5, 5)
+hist(amostra, main = "Distribuído e equilibrado")
+qqnorm(amostra)
+shapiro.test(amostra)
+
+amostra <- rbeta(10000, 2, 8)
+hist(amostra, main = "Enviezamento à Esquerda")
+qqnorm(amostra)
+shapiro.test(amostra)
+
+amostra <- rbeta(10000, 8, 2)
+hist(amostra, main = "Enviezamento à Direita")
+qqnorm(amostra)
+shapiro.test(amostra)
+
+# Media = 8, dp = 2. Objeto < 6?
+pnorm(6, 8, 2)
+
+# Media = 8, dp = 2. Objeto > 6?
+pnorm(6, 8, 2, lower.tail = FALSE)
+# or
+1 - pnorm(6, 8, 2)
+
+# Media = 8, dp = 2. Objeto < 6 ou > 10?
+pnorm(6, 8, 2) + (1 - pnorm(10, 8, 2))
+# or
+pnorm(6, 8, 2) + pnorm(10, 8, 2, lower.tail = FALSE)
+
+# Media = 8, dp = 2. Objeto < 10 e > 8?
+pnorm(10, 8, 2) - pnorm(8, 8, 2)
+
+#
+
+x <- rnorm(100)
+qqnorm(x)
+qqline(x)
+shapiro.test(x)
+
+#
+# Estatística Não Paramétrica
+#
+# Na Estatística Paramétrica os dados seguem um padrão (Ex. Distribuição Normal)
+# 
+
+#
+# Intervalo de Confiança
+#
+
+# IC - margem de erro estimada
+
+# Salário médio de um cientista de dados?
+#   Pesquisados (n) 100
+#   IC              95%
+#   dp              R$1.100,00
+#   média           R$5.800,00
+#   Z               1,96
+#   Margem de erro  +-215,60
+#   Salário         R$5.584,40 e R$6.015,60 para IC de 95%
+#
+#   IC              99%
+#   dp              R$1.100,00
+#   média           R$5.800,00
+#   Z               2,58
+#   Margem de erro  +-283,8
+#   Salário         R$5.516,20 e R$6.083,80 para IC de 99%
+#
+#   Pesquisados (n) 1000
+#   IC              95%
+#   dp              R$1.100,00
+#   média           R$5.800,00
+#   Z               1,96
+#   Margem de erro  +-68,18
+#   Salário         R$5.731,82 e R$5.868,18 para IC de 95%
+
+# Proporção de eleitores que pretendem votar em Maria? 
+#   Pesquisados (n) 1000
+#   IC              95%
+#   650 p/ Maria    p = 650 / 1000 = 0,65
+#   330 p/ João     p = 330 / 1000 = 0,33
+#   20 p/ nenhum    p =  20 / 1000 = 0,02
+#   Z               1,95
+#   p +- 0,03
+#   Votos em Maria  Entre 62 e 68% para IC 95%
+
+#
+# Teste de Hipotese
+#
+
+# Ideal amostra grande
+# Alfa pequeno
+
+#
+# T de Student
+#
+
+# Amostra menor do que 30 elementos
+# Não se sabe o DP
+# Custo: maior variabilidade
+
+# Pesquisa cientistas ganham R$75/h, não sabe-se o dp e n < 30
+# Amostra de 9 cientistas
+# dp da amostra é 10
+# Probabilidade salario < R$80/h
+# u   75
+# S   10 (dp da amostra)
+# n   9
+# t0  8 (grau de liberdade)
+# t = 1,5
+# P = 92,5%
+# Calculadora P = 0,91 ou 91%
+pt(1.5, 8)
+
+# Pesquisa cientistas ganham R$75/h, não sabe-se o dp e n < 30
+# Amostra de 9 cientistas
+# dp da amostra é 10
+# Probabilidade salario > R$80/h
+# u   75
+# S   10 (dp da amostra)
+# n   9
+# t0  8 (grau de liberdade)
+# t = 1,5
+# P = 1 - 0.91 = 0,08
+# Calculadora P = 0,086 ou 8,6%
+pt(1.5, 8, lower.tail = FALSE)
+
+# No R
+# P[X<x] - pt()
+# P[X>x] - lower.tail = FALSE
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
