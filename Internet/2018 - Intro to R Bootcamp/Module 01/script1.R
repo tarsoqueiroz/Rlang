8808280c042e2963c8cc949bd21da9b14268ec6a
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-08-16 11:12:21 -0300

    Intro to R Bootcamp

---
+++ Internet/2018 - Intro to R Bootcamp/Module 01/script1.R
@@ -0,0 +1,75 @@
+mtcars
+?sum
+hist(mtcars$mpg)
+avg_mpg <- mean(mtcars$mpg)
+random_numbers <- runif(25)
+history()
+
+# Getting help
+?sqrt
+example(sqrt)
+
+# Working directory
+getwd()
+
+##############
+# YOUR TURN! #
+##############
+# Set your working directory to the “Intro to R Bootcamp” folder you downloaded 
+# for this class.
+
+
+
+
+# R as a calculator
+4 + 3 / 10 ^ 2
+4 + (3 / 10 ^ 2)
+(4 + 3) / 10 ^ 2
+1 / 17 ^ 7
+1 / 0
+
+# Assignment operator
+x <- 3 # GOOD
+x = 3 # BAD
+
+x <- x + 1
+
+
+##############
+# YOUR TURN! #
+##############
+# compute the provided economic order quantity model
+
+
+
+# Workspace Environment
+# list all objects
+ls()
+
+# remove defined object from the environment
+rm(D)
+
+# removes everything in the working environment -- use with caution!
+rm(list = ls())
+
+
+# installing packages
+install.packages()
+
+
+##############
+# YOUR TURN! #
+##############
+# download the tidyverse and nycflights13 packages
+
+
+
+# Loading packages
+library(tidyverse)
+
+
+
+
+
+
+
---
+++ Internet/2018 - Intro to R Bootcamp/Module 05/script5.R
@@ -0,0 +1,6 @@
+install.packages("devtools")
+devtools::install_github("bradleyboehmke/rbootcamp")
+
+library(rbootcamp)
+get_tutorial("hello")
+get_tutorial("EDA")
