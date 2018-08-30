7f66f8ae5e3f3f4fd04957d0e00ff0e100a79a2d
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-08-30 00:11:20 -0300

    Ending section 3

--- Udemy/R ggplot and Simple Linear Regression/R ggplot and Simple Linear Regression.R
+++ Udemy/R ggplot and Simple Linear Regression/R ggplot and Simple Linear Regression.R
@@ -300,6 +300,122 @@ ggplot()+
              size = 7,
              color = 'red')
 
+#
+# S03C15 - samples along a line
+#
+
+## TASK:        Create fou vertical samples of 100 points each.
+## REQUIREMENT: Means must lie on the line y = 3x + 1
+## X-LOCATIONS: 1, 9, 15, 22
+
+x <- c(0, 25)
+y <- 3 * x + 1
+y
+line <- data.frame(x, y)
+
+ggplot()+
+  geom_line(data = line, 
+            aes(x = x, y = y))
+
+x <- c(1, 9, 15, 22)
+y <- 3 * x + 1
+y
+means <- data.frame(x, y)
+
+ggplot()+
+  geom_line (data = line, 
+             aes(x = x, y = y)) +
+  geom_point(data = means, 
+             aes(x = x, y = y),
+             size = 7,
+             color = 'red')
+
+means
+x <- c(rep(1, 100), rep(9, 100), rep(15, 100), rep(22, 100))
+y <- c(rnorm(100, 4, 10), rnorm(100, 28, 10), rnorm(100, 46, 10), rnorm(100, 67, 10))
+dat <- data.frame(x, y)
+dat
+ggplot()+
+  geom_line (data = line, 
+             aes(x = x, y = y)) +
+  geom_point(data = means, 
+             aes(x = x, y = y),
+             size = 7,
+             color = 'red') +
+  geom_point(data = dat, 
+             aes(x = x, y = y))
+
+#
+# S03C16 - sapply
+#
+
+x <- c(2, 4, 9, 15)
+
+sqrt(x)
+sapply(x, function(x) sqrt(x))
+x^2
+sapply(x, function(x) x ^ 2)
+
+rnorm(1, x, 10)
+sapply(x, function(x) rnorm(1, x, 10))
+
+#
+# S03C17 - cloud of points
+#
+
+## Generate 100 data points in the following away:
+##   The x-coordinates are drawn from a normal population
+##   of mean 10 and standard deviation 5
+##   For each x-value, one y-value is drawn from a normal
+##   population with mean 3x + 1 and standard deviation 10
+
+x <- rnorm(100, 10, 5)
+y <- 3 * x + 1
+means <- data.frame(x, y)
+ggplot()+
+  geom_point(data = means, 
+             aes(x = x, y = y),
+             color = 'red')
+
+x <- c(-5, 25)
+y <- 3 * x + 1
+line <- data.frame(x, y)
+ggplot()+
+  geom_point(data = means, 
+             aes(x = x, y = y),
+             color = 'red') +
+  geom_line(data = line,
+            aes(x = x, y = y)) +
+  scale_x_continuous(limits = c(-10, 30)) +
+  scale_y_continuous(limits = c(-20, 80))
+  
+x <- means$x
+y <- sapply(x, function(x) rnorm(1, 3 * x + 1, 10))
+dat <- data.frame(x, y)
+ggplot()+
+  geom_point(data = means, 
+             aes(x = x, y = y),
+             color = 'red') +
+  geom_line(data = line,
+            aes(x = x, y = y)) +
+  scale_x_continuous(limits = c(-10, 30)) +
+  scale_y_continuous(limits = c(-20, 80)) +
+  geom_point(data = dat, 
+             aes(x = x, y = y))
+ggplot()+
+  scale_x_continuous(limits = c(-10, 30)) +
+  scale_y_continuous(limits = c(-20, 80)) +
+  geom_point(data = dat, 
+             aes(x = x, y = y))
+
+#
+# S04C17 - father and son heights
+#
+
+
+
+
+
 
 
 
