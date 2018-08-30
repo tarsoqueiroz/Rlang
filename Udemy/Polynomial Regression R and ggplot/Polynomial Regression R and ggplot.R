318522c7264f1c850f79c7148b085af89ea14b28
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-08-30 16:58:13 -0300

    First classes

--- Udemy/Polynomial Regression R and ggplot/Polynomial Regression R and ggplot.R
+++ Udemy/Polynomial Regression R and ggplot/Polynomial Regression R and ggplot.R
@@ -5,3 +5,58 @@
 #
 # S01C01 - Introduction
 #
+
+#
+# S01C02 - A review of Functions
+#
+
+## s01c02-a.png
+## s01c02-a.png
+## s01c02-a.png
+
+#
+# S01C03 - Creating Functions in R
+#
+
+f <- function(x) {
+  return(x^2 + 1)
+}
+f(1)
+f(3)
+f(-2)
+
+f <- function(x) {
+  return(6 * x^3 -5 * x + 3)
+}
+f(1)
+f(13)
+
+#
+# S01C04 - Plotting functions with ggplot
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
