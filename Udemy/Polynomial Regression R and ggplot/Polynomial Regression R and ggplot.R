9e91e9eb99994c86dd7e902728da606be29273f1
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-08-30 23:52:00 -0300

    Basics R revision

--- Udemy/Polynomial Regression R and ggplot/Polynomial Regression R and ggplot.R
+++ Udemy/Polynomial Regression R and ggplot/Polynomial Regression R and ggplot.R
@@ -35,13 +35,70 @@ f(13)
 # S01C04 - Plotting functions with ggplot
 #
 
+library(ggplot2)
 
+f <- function(x) {
+  return(5 * x + 1)
+}
+f(1)
 
+x <- c(0, 10)
+x
+axis <- data.frame(x)
+axis
+ggplot() + 
+  stat_function(data = axis,
+                aes(x),
+                fun = f)
+ggplot() + 
+  stat_function(data = data.frame(x = c(0, 10)),
+                aes(x),
+                fun = f)
+ggplot() + 
+  stat_function(data = data.frame(x = c(-10, 10)),
+                aes(x),
+                fun = f)
 
+f <- function(x) {
+  return(x ^ 2)
+}
+f(1)
+ggplot() + 
+  stat_function(data = data.frame(x = c(0, 5)),
+                aes(x),
+                fun = f)
+ggplot() + 
+  stat_function(data = data.frame(x = c(-5, 5)),
+                aes(x),
+                fun = f)
 
+f <- function(x) {
+  return(x ^ 3 - 7 * x + 3)
+}
+f(1)
+ggplot() + 
+  stat_function(data = data.frame(x = c(-5, 5)),
+                aes(x),
+                fun = f)
 
+#
+# S01C05 - Polynomials
+#
 
+## Slope
+## Intersection
+## f(x) = B0.x^0  + B1.x^1  + B2.x^2  + B3.x^3  + ... + Bn.x^n
 
+#
+# S02C05 - Reading in our data
+#
+
+dat <- read.csv(file.choose())
+head(dat)
+
+#
+# S02C06 - Fitting a line
+#
 
 
 
