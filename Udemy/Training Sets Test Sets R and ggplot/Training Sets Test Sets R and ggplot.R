b9429d4e40d845f2bd6392bd5a370f605ab3393b
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-09-04 22:35:02 -0300

    Ending course

--- Udemy/Training Sets Test Sets R and ggplot/Training Sets Test Sets R and ggplot.R
+++ Udemy/Training Sets Test Sets R and ggplot/Training Sets Test Sets R and ggplot.R
@@ -2,6 +2,8 @@
 # Udemy - Training Sets, Test Sets, R and ggplot
 #
 
+library(ggplot2)
+
 #
 # S01C01 - Introduction
 #
@@ -71,13 +73,15 @@ ggplot() +
 # S01C04 - Plotting the least-squares line
 #
 
+install.packages('readxl')
 library(readxl)
 
 train <- read_excel(file.choose())
 test  <- read_excel(file.choose())
 
-Test <- read_excel("Udemy/Training Sets Test Sets R and ggplot/Test.xlsx")
-head(Test)
+# Test <- read_excel("Udemy/Training Sets Test Sets R and ggplot/Test.xlsx")
+# head(Test)
+head(train)
 head(test)
 
 model <- lm(y~x, data = train)
@@ -138,14 +142,124 @@ ggplot() +
 mseq_test <- mean((test$y - predict(model, test))^2)
 mseq_test ## 100.5743
 
+#
+# S01C08 - For loops
+#
 
+for(i in 1:10) {
+  print('hi')
+}
 
+for(i in 1:10) {
+  print(i)
+}
 
+for(i in 1:10) {
+  print(i^2)
+}
 
+#
+# S01C09 - Im revisited
+#
 
+lm(y~x+I(x^2)+I(x^3), data = train)
 
+poly(x, 3, raw = TRUE)
+lm(y~poly(x, 3, raw = TRUE), data = train)
+lm(y~poly(x, 10, raw = TRUE), data = train)
 
+#
+# S01C10 - MSE via a for loop
+#
 
+model <- lm(y~poly(x, 3, raw = TRUE), data = train)
+mse <- mean((test$y-predict(model, test))^2)
+mse
+
+for(i in 1:10) {
+  model <- lm(y~poly(x, i, raw = TRUE), data = train)
+  mse <- mean((test$y-predict(model, test))^2)
+  print(mse)
+}
+
+mse <- numeric()
+for(i in 1:10) {
+  model <- lm(y~poly(x, i, raw = TRUE), data = train)
+  mse[i] <- mean((test$y-predict(model, test))^2)
+}
+mse
+
+#
+# S01C11 - Visualizing the MSE's
+#
+
+y <- mse
+x <- 1:10
+mse_poly <- data.frame(x, y)
+ggplot() +
+  geom_point(data = mse_poly, aes(x = x, y = y), size = 2) +
+  geom_line (data = mse_poly, aes(x = x, y = y)) +
+  xlab('polynomial degree') +
+  ylab('test MSE')
+
+#
+# S01C12 - Functions of two variables
+#
+
+f <- function(x, y) {
+  return(x^2 + y^2)
+}
+f(1, 2)
+f(3, -1)
+
+#
+# S01C13 - For loop inside a function
+#
+
+mse_calc <- function(train, test) {
+  mse <- numeric()
+  for(i in 1:10) {
+    model <- lm(y~poly(x, i, raw = TRUE), data = train)
+    mse[i] <- mean((test$y-predict(model, test))^2)
+  }
+  return(mse)
+}
+
+dat <- read_excel(file.choose())
+
+indices <- sample(1000, 500)
+train <- dat[indices, ]
+test  <- dat[-indices, ]
+mse_calc(train, test)
+
+#
+# S01C14 - Variability of the test MSE
+#
+
+for(i in 1:10) {
+  indices <- sample(1000, 500)
+  train <- dat[indices, ]
+  test  <- dat[-indices, ]
+  print(mse_calc(train, test))
+}
+
+plot <- ggplot()
+x <- 1:10
+for(i in 1:10) {
+  indices <- sample(1000, 500)
+  train <- dat[indices, ]
+  test  <- dat[-indices, ]
+  y <- mse_calc(train, test)
+  mse_poly <- data.frame(x, y)
+  plot <- plot + 
+          geom_point(data = mse_poly, aes(x = x, y = y), size = 3) +
+          geom_line (data = mse_poly, aes(x = x, y = y))
+}
+plot
+
+#
+# S01C15 - Course Wrap-up
+#
 
 
 
